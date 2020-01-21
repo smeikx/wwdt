9547ff25b24a086559921f827a08c68ef5921109
smeikx <gthb@smeikx.com>
2020-01-21 23:27:18 +0100

    update setup for Ubuntu

--- Server/setup.sh
+++ Server/setup.sh
@@ -2,25 +2,48 @@
 
 apt update && apt upgrade && \
 apt install -y \
-	lua \
-	mariadb \ # consider: https://downloads.mariadb.org/mariadb/repositories/#distro=Ubuntu&distro_release=eoan--ubuntu_eoan&mirror=heanet-ltd&version=10.4
+	ffmpeg \
+	lua5.3 \
 	nginx \
-	node \
+	npm \
 	tmux \
 	vim
 
-# Set up Node.js
-npm init
+
+# Maria (10.4)
+
+## import key
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+add-apt-repository 'deb [arch=amd64] http://mariadb.nethub.com.hk/repo/10.4/ubuntu eoan main'
+
+## install server
+apt update
+apt install mariadb-server
+
+## initialise database with init.sql
+
+
+# Node.js (13.x)
+# https://github.com/nodesource/distributions/blob/master/README.md#debian-and-ubuntu-based-distributions
+curl -sL https://deb.nodesource.com/setup_13.x | bash -
+sudo apt-get install -y nodejs
+npm init -y
 ## copy/symlink files to locations
 
-# Set up Nginx
+
+# Nginx
 # https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-18-04
+echo '
+# Nginx
+deb http://nginx.org/packages/ubuntu/ eoan nginx
+deb-src http://nginx.org/packages/ubuntu/ eoan nginx' >> /etc/apt/sources.list
+apt update
+apt install nginx
 ## Adjust Firewall
 ## copy/symlink config files
 ## run at start up
 
 # Set up MariaDB
 # https://linuxize.com/post/how-to-install-mariadb-on-ubuntu-18-04/
-## initialise database with init.sql
 ## config to use socket (in node.js, though)
 
