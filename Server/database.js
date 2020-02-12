fc7d2f9843e2cb60582f07ad76299bb041484ccb
smeikx <gthb@smeikx.com>
2020-02-12 14:15:36 +0100

    simplify module for interacting with databse radically
    The module now only sets up the connection to the database and directly
    exposes the ‘pool’ object to provides all its methods for querying the
    database.

--- Server/database.js
+++ Server/database.js
@@ -4,54 +4,12 @@
 
 const maria = require('mariadb');
 
-const pool = maria.createPool({
+exports.pool = maria.createPool({
 	user: 'tisch',
 	socketPath: '/tmp/mysql.sock',
 	database: 'tisch',
 	connectionLimit: 10,
-	rowsAsArray: true
+	dateStrings: true,
+	rowsAsArray: false
 });
-
-export.query = async (query, placeholders) =>
-{
-	let connection;
-	try
-	{
-		connection = await pool.getConnection();
-		const result = await pool.query(query, placeholders);
-		return result;
-	}
-	catch (error)
-	{
-		throw error;
-	}
-	finally
-	{
-		if (connection) conneciton.end();
-	}
-}
-
-export.batch = async (query, placeholders) =>
-{
-	let connection;
-	try
-	{
-		connection = await pool.getConnection();
-		const result = await pool.batch(query, placeholders);
-		return result;
-	}
-	catch (error)
-	{
-		throw error;
-	}
-	finally
-	{
-		if (connection) conneciton.end();
-	}
-}
-
-export.end = async () =>
-{
-	pool.end();
-}
 
