c49573666b6bb85ad84b8ebfa6e4c4a180437a15
smeikx <gthb@smeikx.com>
2020-02-18 11:48:12 +0100

    create helper class for iterating over a GQL request’s selections

---
+++ Server/src/selection-iterable.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// an Iterable for extracting selections
+// it keeps track of (nested) fragments
+// TODO: handle multiple FieldNodes entries (https://github.com/graphql/graphql-js/issues/2304)
+
+class SelectionIterable
+{
+	// needs either ‘info’ OR ‘selections’ and ‘fragments’
+	constructor ({info, selections, fragments})
+	{
+		if (info)
+		{
+			this.selections = info.fieldNodes[0].selectionSet.selections;
+			this.fragments = info.fragments;
+		}
+		else
+		{
+			this.selections = selections;
+			this.fragments = fragments;
+		}
+	}
+
+	* [Symbol.iterator] ()
+	{
+		for (const selection of this.selections)
+		{
+			if (selection.kind == 'FragmentSpread')
+			{
+				const fragment = new SelectionIterable({
+					selections: this.fragments[selection.name.value].selectionSet.selections,
+					fragments: this.fragments
+				});
+				for (const frag_selection of fragment)
+					yield frag_selection;
+			}
+			else
+				yield selection.name.value;
+		}
+	}
+}
+
+exports.SelectionIterable = SelectionIterable;
